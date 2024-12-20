28b8f33e0543860c7b5c1d5cbeac1895fd1af3ee
Venkadakrishnasharma <101462622+Thanujaavks@users.noreply.github.com>
2024-12-20 17:25:28 +0530

    changes

--- wowdash-file/src/components/ViewDetailsLayer.jsx
+++ wowdash-file/src/components/ViewDetailsLayer.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
-import axios from "axios";
 import axiosInstance from "../hook/axiosInstance";
 
 const ViewDetailsLayer = () => {
