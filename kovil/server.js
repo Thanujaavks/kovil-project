6458f05012b3dbb29238f22dfbf7c7d8b4cb1e6d
Venkadakrishnasharma <101462622+Thanujaavks@users.noreply.github.com>
2024-12-20 17:30:03 +0530

    changes

--- kovil/server.js
+++ kovil/server.js
@@ -16,7 +16,7 @@ const port = process.env.PORT || 5000;  // Set the port for the server, using an
 
 
 const app = express(); // Initialize the Express application
-const customerRoutes = require('./routes/customerRoutes');
+
 
 // Swagger
 // Load env variables
@@ -28,7 +28,6 @@ app.use(morgan('dev')); // Log HTTP requests in 'dev' format
 app.use(cookieParser()); // Parse cookies attached to client requests
 app.use(express.urlencoded({extended: true})); // Parse URL-encoded data
 app.use(routes); // Use the imported routes for handling application endpoints
-app.use(customerRoutes);
 
 
 // Define a base route for testing the server
